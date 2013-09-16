cbe45336999285aa05b0ad52eaedee3a5ec7a722
palle torsson <palle.torsson@gmail.com>
2013-09-16 12:10:55 +0200

    unity3d ball data simulation missing real data

---
+++ unity3d/Assets/GUI_Scripts.js
@@ -0,0 +1,43 @@
+﻿private var textFieldString = "Socket string data";
+
+private var myTCP; 
+
+var s : GameObject;
+s = GameObject.Find("Sphere_l_1");
+
+public var intc : int = 1;
+
+function Awake() {
+ 		myTCP = gameObject.AddComponent('s_TCP');
+}
+
+function OnGUI () {
+ 	if(myTCP.socketReady==false) {
+  		if (GUI.Button (Rect (20,10,80,20),"Connect")) {
+   			myTCP.setupSocket();
+  		}
+ 	} else {
+  		myTCP.maintainConnection();
+
+  	if (GUI.Button (Rect (20,40,80,20), "change")) {
+   		myTCP.writeSocket(" The is from Level 1 Button");
+   		textFieldString=myTCP.readSocket();
+   		intc = intc - parseInt(textFieldString); 
+   		s.transform.localScale = Vector3(10,10,10);  
+   		//intc = parseInt(textFieldString);
+   		}
+   		
+  if (GUI.Button (Rect (20,70,80,20), "get data")) {
+   		myTCP.writeSocket(" The is from Level 2 Button");
+   		textFieldString=myTCP.readSocket();
+  }
+  
+  textFieldString = GUI.TextField (Rect (25, 100, 300, 30), textFieldString);
+  
+  if (GUI.Button (Rect (20,140,80,20),"Disconnect")) {
+   		myTCP.closeSocket();
+   		textFieldString = "Socket Disconnected...";
+  }
+ }
+}
+
---
+++ unity3d/Assets/NewBehaviourScript.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+var ct : int = 2; 
+public var countdown : float = 1f;
+public var r : float = 1f;
+public var temp : float = 1f;
+
+function Update () {
+	countdown -= Time.deltaTime;
+    if(countdown <= 0.5f) {
+		ct++;
+		r = Random.Range(1.0, 2.0);
+		temp = ct - r;
+	    transform.localScale = Vector3(temp,temp,temp);
+	if (ct == 5) {
+			ct = 4;	
+		}
+		countdown = 2f;
+	}
+}
